c173ffa2671c93b525e9c08866b3c7487db0637a
Shelvacu <shelvacu@gmail.com>
2019-06-19 14:01:08 -0700

    what if I used the call that was 20-30ish lines under where I was writing the code :thinking:

--- fuckmewhatdowecallit.s
+++ fuckmewhatdowecallit.s
@@ -18,12 +18,10 @@ start:
         mov ah, 00h		; int 16h Read key press
         int 16h			; AH is the keyboard scan code and AL is the ascii code
         
-        mov ah, 0Ah             ; Now we're going to write the character we just got; wow!
+        mov ah, 0Eh             ; Now we're going to write the character we just got; wow!
         mov bh, 0h              ; page number (???)
-        mov cx, 1h              ; number of times to print
+        mov bl, 0Fh             ; white-on-black color
         int 10h
-
-        call move_cursor_forward
 
         dec dl
         cmp dl, 0
@@ -48,25 +46,6 @@ print_string:			; Routine: output string in SI to screen
 
 .done:
 	ret
-
-move_cursor_forward:
-        push ax
-        push bx
-        push cx
-        push dx
-        mov ah, 03h             ; Get cursor position and shape (shape? what?)
-        mov bh, 0h              ; page 0
-        int 10h                 ; DH is row, DL is column
-        inc dl                  ; Move cursor forward one
-        mov ah, 02h             ; set cursor position
-        int 10h
-
-.done:
-        pop dx
-        pop cx
-        pop bx
-        pop ax
-        ret
 
 	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
 	dw 0xAA55		     ; The standard PC boot signature
