8c8738f98c4da9c3a756cdd691b9da507019f5da
Shelvacu <shelvacu@gmail.com>
2019-06-19 13:59:22 -0700

    committing my idiocy so I can never forget it

--- fuckmewhatdowecallit.s
+++ fuckmewhatdowecallit.s
@@ -13,6 +13,23 @@ start:
 	mov si, text_string	; Put string position into SI
 	call print_string	; Call our string-printing routine
 
+        mov dl, 10h
+.loop:
+        mov ah, 00h		; int 16h Read key press
+        int 16h			; AH is the keyboard scan code and AL is the ascii code
+        
+        mov ah, 0Ah             ; Now we're going to write the character we just got; wow!
+        mov bh, 0h              ; page number (???)
+        mov cx, 1h              ; number of times to print
+        int 10h
+
+        call move_cursor_forward
+
+        dec dl
+        cmp dl, 0
+        jne .loop
+        
+
 	jmp $			    ; Jump here - infinite loop!
 
 
@@ -32,6 +49,26 @@ print_string:			; Routine: output string in SI to screen
 .done:
 	ret
 
+move_cursor_forward:
+        push ax
+        push bx
+        push cx
+        push dx
+        mov ah, 03h             ; Get cursor position and shape (shape? what?)
+        mov bh, 0h              ; page 0
+        int 10h                 ; DH is row, DL is column
+        inc dl                  ; Move cursor forward one
+        mov ah, 02h             ; set cursor position
+        int 10h
+
+.done:
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+        ret
 
 	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
 	dw 0xAA55		     ; The standard PC boot signature
+
+        
